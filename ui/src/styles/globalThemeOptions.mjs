f877e338299b37b7aa849186215cc7f0b949ef81
Benjamin Pernot <benjamin.pernot195@gmail.com>
2024-09-15 16:05:35 +0100

    css changes and custom theme

---
+++ ui/src/styles/globalThemeOptions.mjs
@@ -0,0 +1,14 @@
+export const globalThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#8dc550",
+    },
+    secondary: {
+      main: "#48aaff",
+    },
+    background: {
+      default: "#eeeeee",
+    },
+  },
+};
