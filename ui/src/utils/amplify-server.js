502baff2fbb27f1db0fc4e2503e224c9ff0308a8
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-04 19:38:55 +0000

    getting the user from the user server function

---
+++ ui/src/utils/amplify-server.js
@@ -0,0 +1,35 @@
+import { authConfig } from "@/src/app/amplify-cognito-config";
+import { createServerRunner } from "@aws-amplify/adapter-nextjs";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth/server";
+import { USER_ROLES } from "../lib/constants.mjs";
+import { getHighestUserGroup } from "../lib/commonFunctions.mjs";
+
+export const { runWithAmplifyServerContext } = createServerRunner({
+  config: {
+    Auth: authConfig,
+  },
+});
+
+export async function authenticatedUser(context) {
+  return await runWithAmplifyServerContext({
+    nextServerContext: context,
+    operation: async (contextSpec) => {
+      try {
+        const session = await fetchAuthSession(contextSpec);
+        if (!session.tokens) {
+          return { role: USER_ROLES.GUEST };
+        }
+        const user = {
+          ...(await getCurrentUser(contextSpec)),
+          role: USER_ROLES.USER,
+        };
+        const groups = session.tokens.accessToken.payload["cognito:groups"];
+        user.role = getHighestUserGroup(groups);
+
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+}
