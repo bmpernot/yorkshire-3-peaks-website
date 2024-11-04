1714880c74b547c8cde340f303434b0360984a01
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-04 19:38:24 +0000

    making a function to wrap the app

---
+++ ui/src/app/amplify-cognito-config.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { Amplify } from "aws-amplify";
+
+export const authConfig = {
+  Cognito: {
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+  },
+};
+
+Amplify.configure({ Auth: authConfig }, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
