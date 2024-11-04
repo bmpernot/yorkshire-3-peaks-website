0469b41b3b03e14f4010b45d637d6bc7e0ef7460
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-04 19:37:57 +0000

    creating a get user method to find the user from the cookies (session data)

---
+++ ui/src/app/hooks/use-auth-user.js
@@ -0,0 +1,31 @@
+import { fetchAuthSession, fetchUserAttributes, getCurrentUser } from "aws-amplify/auth";
+import { useEffect, useState } from "react";
+import { USER_ROLES } from "../../lib/constants.mjs";
+import { getHighestUserGroup } from "@/src/lib/commonFunctions.mjs";
+
+export default function useAuthUser() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    async function getUser() {
+      const session = await fetchAuthSession();
+      if (!session.tokens) {
+        setUser({ role: USER_ROLES.GUEST });
+        return;
+      }
+      const user = {
+        ...(await getCurrentUser()),
+        ...(await fetchUserAttributes()),
+        role: USER_ROLES.USER,
+      };
+      const groups = session.tokens.accessToken.payload["cognito:groups"];
+      user.role = getHighestUserGroup(groups);
+
+      setUser(user);
+    }
+
+    getUser();
+  }, []);
+
+  return user;
+}
