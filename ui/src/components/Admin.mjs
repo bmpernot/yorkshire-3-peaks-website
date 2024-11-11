e3332d280e95ddd5390b52888ab2139f1442d171
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-11 19:23:44 +0000

    made some todo for future tickets

--- ui/src/components/Admin.mjs
+++ ui/src/components/Admin.mjs
@@ -1,6 +1,9 @@
 "use client";
 
 function Admin() {
+  // TODO - need to make a button that makes a user a admin
+
+  // TODO - create a lambda function that can update user groups - for now it is manual
   return <h1>Admin</h1>;
 }
 
