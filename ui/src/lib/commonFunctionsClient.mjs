bd821d336a6a4fa4c495b3e89520c51e6901641d
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-16 10:40:51 +0000

    made a global validate input

--- ui/src/lib/commonFunctionsClient.mjs
+++ ui/src/lib/commonFunctionsClient.mjs
@@ -4,4 +4,40 @@ const openEmail = () => {
   window.location = "mailto:yorkshirepeaks@gmail.com";
 };
 
-export { openEmail };
+function validateInputs(setErrors, formValidations) {
+  let isValid = true;
+
+  formValidations.forEach((formValidation) => {
+    if (formValidation.validation(...formValidation.element)) {
+      setErrors((errors) => {
+        let newErrors = { ...errors };
+        if (!errors[formValidation.field].includes(formValidation.errorMessage)) {
+          newErrors = {
+            ...errors,
+            [formValidation.field]: [...errors[formValidation.field], formValidation.errorMessage],
+          };
+        }
+        isValid = errors[formValidation.field].length > 0 ? false : true;
+        return newErrors;
+      });
+    } else {
+      setErrors((errors) => {
+        let newErrors = { ...errors };
+        if (errors[formValidation.field].includes(formValidation.errorMessage)) {
+          const updatedFieldErrors = errors[formValidation.field].filter(
+            (element) => element !== formValidation.errorMessage,
+          );
+          newErrors = {
+            ...errors,
+            [formValidation.field]: updatedFieldErrors,
+          };
+        }
+        return newErrors;
+      });
+    }
+  });
+
+  return isValid;
+}
+
+export { openEmail, validateInputs };
