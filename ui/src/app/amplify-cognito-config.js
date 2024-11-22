ad766eb1b127c8ea9fe8652c5683162671ca556a
Ben Pernot <benjamin.pernot195@gmail.com>
2024-11-22 14:55:45 +0000

    adding an api config to the amplify server

--- ui/src/app/amplify-cognito-config.js
+++ ui/src/app/amplify-cognito-config.js
@@ -6,10 +6,25 @@ export const authConfig = {
   Cognito: {
     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
     userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
   },
 };
 
-Amplify.configure({ Auth: authConfig }, { ssr: true });
+export const apiConfig = {
+  endpoints: [
+    {
+      name: "api",
+      endpoint: process.env.NEXT_PUBLIC_API_URL,
+      region: process.env.NEXT_PUBLIC_AWS_REGION,
+      custom_header: async () => {
+        const session = await Amplify.Auth.currentSession();
+        return { Authorization: `Bearer ${session.getIdToken().getJwtToken()}` };
+      },
+    },
+  ],
+};
+
+Amplify.configure({ Auth: authConfig, API: apiConfig }, { ssr: true });
 
 export default function ConfigureAmplifyClientSide() {
   return null;
