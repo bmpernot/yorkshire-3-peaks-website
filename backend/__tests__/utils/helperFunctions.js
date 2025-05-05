54788d1fbf42214af1a81271a6cae0550da037b6
Ben Pernot <benjamin.pernot195@gmail.com>
2025-05-05 13:59:48 +0000

    remove unnecessary comments

--- backend/__tests__/utils/helperFunctions.js
+++ backend/__tests__/utils/helperFunctions.js
@@ -32,30 +32,30 @@ function generateGetAllUsersEvent({ fields = "", userRole, eventOverrides }) {
     requestContext: {
       authorizer: {
         claims: {
-          sub: "12345678-1234-1234-1234-123456789012", // Unique identifier for the user
-          email: "user@example.com", // User's email address
-          email_verified: "true", // Indicates if the user's email is verified
-          "custom:ice_number": "01234567890", // Custom attribute for the user's role
-          "custom:notify": "true", // Custom attribute for the user's role
-          given_name: "John", // User's first name
-          family_name: "Doe", // User's last name
-          phone_number: "01234567890", // User's phone number
-          phone_number_verified: "true", // Indicates if the user's phone number is verified
-          aud: "client_id_abc123", // Cognito app client ID
-          event_id: "a12bc34d-567e-89fa-bc12-3456789defgh", // Unique event ID
-          token_use: "id", // Indicates the type of token (e.g., id or access)
-          auth_time: "1700000000", // Timestamp when the user was authenticated
-          exp: "1700100000", // Token expiration timestamp
-          iss: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ABCDEFGHI", // Issuer of the token (Cognito user pool URL)
-          iat: "1700000000", // Token issue timestamp
-          username: "user@example.com", // Cognito username
+          sub: "12345678-1234-1234-1234-123456789012",
+          email: "user@example.com",
+          email_verified: "true",
+          "custom:ice_number": "01234567890",
+          "custom:notify": "true",
+          given_name: "John",
+          family_name: "Doe",
+          phone_number: "01234567890",
+          phone_number_verified: "true",
+          aud: "client_id_abc123",
+          event_id: "a12bc34d-567e-89fa-bc12-3456789defgh",
+          token_use: "id",
+          auth_time: "1700000000",
+          exp: "1700100000",
+          iss: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ABCDEFGHI",
+          iat: "1700000000",
+          username: "user@example.com",
         },
       },
     },
   };
 
   if (userRole) {
-    event.requestContext.authorizer.claims["cognito:groups"] = userRole; // User's group (e.g., Admin, Organiser)
+    event.requestContext.authorizer.claims["cognito:groups"] = userRole;
   }
 
   return { ...event, ...eventOverrides };
