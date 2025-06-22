f6e16483e76f39811faaaf9c8c9a82735762309e
Ben Pernot <benjamin.pernot195@gmail.com>
2025-06-22 12:28:58 +0000

    promotional page todo list

--- ui/src/components/Promotion.mjs
+++ ui/src/components/Promotion.mjs
@@ -1,7 +1,26 @@
-"use client";
-
 function Promotion() {
-  return <h1>Promotion</h1>;
+  return (
+    <>
+      <p>
+        Welcome everyone to help advertise it, everyone is welcome. you can up with your own advertisement/announcements
+        or you can use the pre made ones below. Important - don't forget to swap the date with the actually event date
+      </p>
+      <h1>Announcements</h1>
+      <h2>Recruitment</h2>
+      <h2>Reminder</h2>
+      <h2>Notice</h2>
+      <h1>Advertisements</h1>
+      <h2>Flyers</h2>
+      <h2>Posters</h2>
+    </>
+  );
 }
+
+// TODO - make a flyer
+// TODO - make a poster
+// TODO - get the announcements i made in the past
+// TODO - sort out the wording
+// TODO - create collapsable components so the user only sees what they want to see
+// TODO - allow the flyers and posters to be downloadable and previewable
 
 export default Promotion;
