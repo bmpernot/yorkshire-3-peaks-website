38fb76fd03d86ae7f6b7ca98d3bc34acc4507db6
Ben Pernot <benjamin.pernot195@gmail.com>
2025-06-22 12:33:54 +0000

    rules todo list

--- ui/src/components/Rules.mjs
+++ ui/src/components/Rules.mjs
@@ -1,7 +1,8 @@
-"use client";
-
 function Rules() {
   return <h1>Rules</h1>;
 }
+
+// TODO - make the rule a PDF document that can be downloaded
+// TODO - display the rules and equipment requirements is an easy and understandable way
 
 export default Rules;
