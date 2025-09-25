a85e5bfd2e4699bc54c0684ce97c4dba0391af42
Ben Pernot <benjamin.pernot@bt.com>
2025-09-25 08:31:33 +0000

    fixed results table columns

--- ui/src/components/Results.mjs
+++ ui/src/components/Results.mjs
@@ -2,7 +2,7 @@
 
 import { useEffect, useState, useCallback } from "react";
 import { get } from "aws-amplify/api";
-import { Box, FormControl, InputLabel, MenuItem, Select, IconButton, Paper } from "@mui/material";
+import { Box, FormControl, InputLabel, MenuItem, Select, IconButton, Paper, Button } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { Refresh } from "@mui/icons-material";
 import Loading from "./common/Loading.mjs";
@@ -142,56 +142,130 @@ function Events({ events, selectedEvent, setSelectedEvent, fetchEvents }) {
 }
 
 function Entries({ entries, fetchEntries, selectedEvent }) {
-  console.log("entries", entries);
-  console.log("stuff", generateColumns(entries));
-
   return (
-    <Box paddingTop="1rem" display="flex" alignItems="center">
-      <Paper>
-        <DataGrid
-          rows={entries}
-          columns={[]}
-          initialState={{ pagination: { paginationModel: { page: 0, pageSize: 5 } } }}
-          pageSizeOptions={entries.length > 5 ? [5, 10] : []}
-          getRowId={(entry) => entry.teamId}
-        />
-      </Paper>
-      <IconButton
+    <>
+      <Box paddingTop="1rem" display="flex" alignItems="center" maxWidth="90vw" maxHeight="70vh" overflow="auto">
+        <Paper>
+          <DataGrid
+            rows={entries}
+            columns={generateColumns(entries)}
+            initialState={{ pagination: { paginationModel: { page: 0, pageSize: 5 } } }}
+            pageSizeOptions={entries.length > 5 ? [5, 10] : []}
+            getRowId={(entry) => entry.teamId}
+          />
+        </Paper>
+      </Box>
+      <Button
         id="refresh-events"
+        variant="contained"
+        endIcon={<Refresh />}
         onClick={() => {
           fetchEntries({ selectedEvent, forceRefresh: true });
         }}
       >
-        <Refresh />
-      </IconButton>
-    </Box>
+        Refresh Events
+      </Button>
+    </>
   );
 }
 
-// TODO - need to finish this
-// TODO - auto generate the columns out of the unique keys from the entries then sprit out the data we know we dont want and create new data of the diffs between each checkpoint but make it hidden column
-
 function generateColumns(entries) {
   const keys = [...new Set(entries.flatMap((entry) => Object.keys(entry)))];
-  const numberOfCheckpoint = keys.filter((key) => key.startsWith("checkpoint")).length;
+  const checkpoints = keys.filter((key) => key.startsWith("checkpoint")).sort();
   const columns = [
-    { field: "teamName", headerName: "Team name" },
-    { field: "start", headerName: "Start" },
-    ...generateCheckpointColumnObjects({ numberOfCheckpoint }),
-    { field: "end", headerName: "End" },
-    { field: "time", headerName: "Time" },
+    { field: "teamName", headerName: "Team name", width: 200 },
+    {
+      field: "start",
+      headerName: "Start",
+      width: 120,
+      valueGetter: (_, row) => {
+        const date = new Date(row.start);
+        return timeStamp(date);
+      },
+    },
+    ...generateCheckpointColumnObjects({ checkpoints }),
+    {
+      field: "end",
+      headerName: "End",
+      width: 100,
+      valueGetter: (_, row) => {
+        const date = new Date(row.end);
+        return timeStamp(date);
+      },
+    },
+    {
+      field: "time",
+      headerName: "Time",
+      width: 100,
+      valueGetter: (_, row) => {
+        const date = new Date(row.time);
+        return timeStamp(date);
+      },
+    },
   ];
   return columns;
 }
 
-function generateCheckpointColumnObjects({ numberOfCheckpoint }) {
+function generateCheckpointColumnObjects({ checkpoints }) {
   const columns = [];
-
-  columns.push({});
-  for (let index = 0; index < numberOfCheckpoint; index++) {
-    columns.push({});
-    columns.push({});
+  if (checkpoints.length === 0) {
+    return columns;
+  } else {
+    columns.push({
+      field: `start-to-${checkpoints[0]}`,
+      headerName: `Start to Checkpoint ${checkpoints[0].split("checkpoint")[1]}`,
+      width: 205,
+      valueGetter: (_, row) => {
+        const date = new Date(new Date(row[checkpoints[0]]).valueOf() - new Date(row.start).valueOf());
+        return timeStamp(date);
+      },
+    });
   }
+
+  for (let index = 0; index < checkpoints.length; index++) {
+    columns.push({
+      field: `${checkpoints[index]}`,
+      headerName: `Checkpoint ${checkpoints[index].split("checkpoint")[1]}`,
+      width: 160,
+      valueGetter: (_, row) => {
+        const date = new Date(row[checkpoints[index]]);
+        return timeStamp(date);
+      },
+    });
+
+    if (index === checkpoints.length - 1) {
+      columns.push({
+        field: `${checkpoints[index]}-to-end`,
+        headerName: `Checkpoint ${checkpoints[index].split("checkpoint")[1]} to End`,
+        width: 200,
+        valueGetter: (_, row) => {
+          const date = new Date(new Date(row.end).valueOf() - new Date(row[checkpoints[index]]).valueOf());
+          return timeStamp(date);
+        },
+      });
+    } else {
+      columns.push({
+        field: `${checkpoints[index]}-to-${checkpoints[index + 1]}`,
+        headerName: `Checkpoint ${checkpoints[index].split("checkpoint")[1]} to Checkpoint ${checkpoints[index + 1].split("checkpoint")[1]}`,
+        width: 230,
+        valueGetter: (_, row) => {
+          const date = new Date(
+            new Date(row[checkpoints[index + 1]]).valueOf() - new Date(row[checkpoints[index]]).valueOf(),
+          );
+          return timeStamp(date);
+        },
+      });
+    }
+  }
+
+  return columns;
+}
+
+function timeStamp(date) {
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 export default Results;
