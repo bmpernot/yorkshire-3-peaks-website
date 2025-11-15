05e84036c426f59ef2424db9a993da9bc837e156
Benjamin Pernot <benjamin.pernot195@gmail.com>
2025-11-15 10:53:47 +0000

    trying to improve vitest

--- ui/vitest.config.js
+++ ui/vitest.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,9 +12,16 @@ export default defineConfig({
     },
   },
   test: {
+    testTimeout: 0,
     environment: "jsdom",
     setupFiles: ["./__tests__/config/setup.js"],
     globals: true,
     include: ["__tests__/**/*.test.jsx"],
+    exclude: ["node_modules", ".next", "cypress", "public", "package*"],
+    deps: {
+      optimizer: {
+        web: { include: ["@mui/material", "@mui/system", "@mui/icons-material", "@emotion/react", "@emotion/styled"] },
+      },
+    },
   },
 });
