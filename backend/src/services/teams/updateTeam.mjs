c7547464729b6da3238b42d81cdb366ac95980d9
Benjamin Pernot <benjamin.pernot195@gmail.com>
2025-11-23 14:42:30 +0000

    add try catches and pull data out of query

--- backend/src/services/teams/updateTeam.mjs
+++ backend/src/services/teams/updateTeam.mjs
@@ -17,119 +17,129 @@ const updateTeamFunction = async (teamId, eventId, actions) => {
   const deleteAll = actions.some((action) => action.action === "delete" && action.type === "entry");
 
   if (deleteAll) {
-    const members = await ddbDocClient.send(
-      new QueryCommand({
-        TableName: teamMembersTableName,
-        IndexName: "EventIdIndex",
-        KeyConditionExpression: "eventId = :eventId",
-        FilterExpression: "#teamId = :teamId",
-        ExpressionAttributeNames: {
-          "#teamId": "teamId",
-        },
-        ExpressionAttributeValues: {
-          ":teamId": teamId,
-          ":eventId": eventId,
-        },
-      }),
-    );
-
-    const deletes = [
-      {
-        Delete: {
-          TableName: teamsTableName,
-          Key: { teamId },
-        },
-      },
-      {
-        Delete: {
-          TableName: entriesTableName,
-          Key: { teamId, eventId },
-        },
-      },
-      ...members.map((member) => ({
-        Delete: {
+    try {
+      const membersData = await ddbDocClient.send(
+        new QueryCommand({
           TableName: teamMembersTableName,
-          Key: { teamId, userId: member.userId },
+          IndexName: "EventIdIndex",
+          KeyConditionExpression: "eventId = :eventId",
+          FilterExpression: "#teamId = :teamId",
+          ExpressionAttributeNames: {
+            "#teamId": "teamId",
+          },
+          ExpressionAttributeValues: {
+            ":teamId": teamId,
+            ":eventId": eventId,
+          },
+        }),
+      );
+
+      const members = membersData.Items || [];
+
+      const deletes = [
+        {
+          Delete: {
+            TableName: teamsTableName,
+            Key: { teamId },
+          },
         },
-      })),
-    ];
+        {
+          Delete: {
+            TableName: entriesTableName,
+            Key: { teamId, eventId },
+          },
+        },
+        ...members.map((member) => ({
+          Delete: {
+            TableName: teamMembersTableName,
+            Key: { teamId, userId: member.userId },
+          },
+        })),
+      ];
 
-    await ddbDocClient.send(new TransactWriteCommand({ TransactItems: deletes }));
+      await ddbDocClient.send(new TransactWriteCommand({ TransactItems: deletes }));
 
-    return { action: "deleted", teamId, eventId };
-  }
-
-  const transactItems = [];
-
-  for (const { action, type, newValues } of actions) {
-    switch (type) {
-      case "teamName":
-        if (action === "modify") {
-          transactItems.push({
-            Update: {
-              TableName: teamsTableName,
-              Key: { teamId },
-              UpdateExpression: "SET teamName = :teamName",
-              ExpressionAttributeValues: { ":teamName": newValues.teamName },
-            },
-          });
-        }
-        break;
-
-      case "teamMembers":
-        if (action === "add") {
-          transactItems.push({
-            Put: {
-              TableName: teamMembersTableName,
-              Item: {
-                teamId,
-                eventId,
-                userId: newValues.userId,
-                additionalRequirements: newValues.additionalRequirements || null,
-                willingToVolunteer: newValues.willingToVolunteer || false,
-              },
-            },
-          });
-        }
-
-        if (action === "modify") {
-          transactItems.push({
-            Update: {
-              TableName: teamMembersTableName,
-              Key: { teamId, userId: newValues.userId },
-              UpdateExpression:
-                "SET #additionalRequirements = :additionalRequirements, #willingToVolunteer = :willingToVolunteer",
-              ExpressionAttributeValues: {
-                ":additionalRequirements": newValues.additionalRequirements ?? null,
-                ":willingToVolunteer": newValues.willingToVolunteer ?? false,
-              },
-              ExpressionAttributeNames: {
-                "#additionalRequirements": "additionalRequirements",
-                "#willingToVolunteer": "willingToVolunteer",
-              },
-            },
-          });
-        }
-
-        if (action === "delete") {
-          transactItems.push({
-            Delete: {
-              TableName: teamMembersTableName,
-              Key: { teamId, userId: newValues.userId },
-            },
-          });
-        }
-        break;
+      return { action: "deleted", teamId, eventId };
+    } catch (error) {
+      throw new Error("failed to delete entry", { cause: error });
     }
   }
 
-  if (transactItems.length === 0) {
-    return { action: "null", teamId, eventId };
+  try {
+    const transactItems = [];
+
+    for (const { action, type, newValues } of actions) {
+      switch (type) {
+        case "teamName":
+          if (action === "modify") {
+            transactItems.push({
+              Update: {
+                TableName: teamsTableName,
+                Key: { teamId },
+                UpdateExpression: "SET teamName = :teamName",
+                ExpressionAttributeValues: { ":teamName": newValues.teamName },
+              },
+            });
+          }
+          break;
+
+        case "teamMembers":
+          if (action === "add") {
+            transactItems.push({
+              Put: {
+                TableName: teamMembersTableName,
+                Item: {
+                  teamId,
+                  eventId,
+                  userId: newValues.userId,
+                  additionalRequirements: newValues.additionalRequirements || null,
+                  willingToVolunteer: newValues.willingToVolunteer || false,
+                },
+              },
+            });
+          }
+
+          if (action === "modify") {
+            transactItems.push({
+              Update: {
+                TableName: teamMembersTableName,
+                Key: { teamId, userId: newValues.userId },
+                UpdateExpression:
+                  "SET #additionalRequirements = :additionalRequirements, #willingToVolunteer = :willingToVolunteer",
+                ExpressionAttributeValues: {
+                  ":additionalRequirements": newValues.additionalRequirements ?? null,
+                  ":willingToVolunteer": newValues.willingToVolunteer ?? false,
+                },
+                ExpressionAttributeNames: {
+                  "#additionalRequirements": "additionalRequirements",
+                  "#willingToVolunteer": "willingToVolunteer",
+                },
+              },
+            });
+          }
+
+          if (action === "delete") {
+            transactItems.push({
+              Delete: {
+                TableName: teamMembersTableName,
+                Key: { teamId, userId: newValues.userId },
+              },
+            });
+          }
+          break;
+      }
+    }
+
+    if (transactItems.length === 0) {
+      return { action: "null", teamId, eventId };
+    }
+
+    await ddbDocClient.send(new TransactWriteCommand({ TransactItems: transactItems }));
+
+    return { action: "updated", teamId, eventId };
+  } catch (error) {
+    throw new Error("failed to update team", { cause: error });
   }
-
-  await ddbDocClient.send(new TransactWriteCommand({ TransactItems: transactItems }));
-
-  return { action: "updated", teamId, eventId };
 };
 
 export default updateTeamFunction;
