cf39d0c48d5f0df796da09c50f6b5470af891f9d
Benjamin Pernot <benjamin.pernot195@gmail.com>
2025-11-23 14:43:55 +0000

    fix data for when the admin is using the function

--- backend/src/services/teams/getTeams.mjs
+++ backend/src/services/teams/getTeams.mjs
@@ -13,7 +13,6 @@ const eventsTableName = process.env.EVENTS_TABLE_NAME || "EventsTable";
 const usersTableName = process.env.USERS_TABLE_NAME || "UsersTable";
 
 const getTeamsFunction = async (teamIds, userRole) => {
-  // TODO - if is organiser or admin get user information from cognito and include phone number and ICE number
   try {
     if (!Array.isArray(teamIds) || teamIds.length === 0) {
       throw new Error("teamIds must be a non-empty array");
@@ -64,12 +63,19 @@ const getTeamsFunction = async (teamIds, userRole) => {
       events = data[eventsTableName];
       users = data[usersTableName];
     } else {
-      const [data, usersData] = Promise.all([
+      const [data, usersData] = await Promise.all([
         ddbDocClient.send(new BatchGetCommand(secondParams)),
         getUsersFunction("sub,given_name,family_name,email,ice_number,phone_number", userIdKeys),
       ]);
       events = data.Responses[eventsTableName];
-      users = usersData;
+      users = usersData.map((userData) => ({
+        userId: userData.sub,
+        email: userData.email,
+        firstName: userData.given_name,
+        lastName: userData.family_name,
+        iceNumber: userData["custom:ice_number"],
+        number: userData.phone_number,
+      }));
     }
 
     const teamObjects = teamIds.map((teamId) => {
@@ -78,7 +84,7 @@ const getTeamsFunction = async (teamIds, userRole) => {
       const teamMemberIds = teamMembersEventInformation.map((teamMember) => teamMember.userId);
       const entry = entries.find((entry) => entry.teamId === teamId);
       const event = events.find((event) => event.eventId === entry.eventId);
-      const teamMembersPersonalInformation = users.filter((user) => teamMemberIds.include(user.userId));
+      const teamMembersPersonalInformation = users.filter((user) => teamMemberIds.includes(user.userId));
 
       return {
         teamId: teamId,
@@ -90,12 +96,9 @@ const getTeamsFunction = async (teamIds, userRole) => {
           );
 
           return {
-            userId: teamMemberId,
-            firstName: teamMemberPersonalInformation.firstName,
-            lastName: teamMemberPersonalInformation.lastName,
-            email: teamMemberPersonalInformation.email,
-            additionalRequirements: teamMemberEventInformation.additionalRequirements,
-            willingToVolunteer: teamMemberEventInformation.willingToVolunteer,
+            ...teamMemberPersonalInformation,
+            additionalRequirements: teamMemberEventInformation.additionalRequirements || null,
+            willingToVolunteer: teamMemberEventInformation.willingToVolunteer || false,
           };
         }),
         volunteer: entry.volunteer,
