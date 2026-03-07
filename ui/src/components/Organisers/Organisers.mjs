b2223e7fedc28cedfeef9b3a7c38dd13d1a5be83
Benjamin Pernot <benjamin.pernot195@gmail.com>
2026-03-07 11:51:45 +0000

    added component to organisers page

--- ui/src/components/Organisers/Organisers.mjs
+++ ui/src/components/Organisers/Organisers.mjs
@@ -1,10 +1,30 @@
 "use client";
 
-function Organisers() {
-  // TODO - need to make a button that makes a user a organiser
+import EventRegistrationForm from "./EventRegistrationForm.mjs";
+import { Typography, Grid, Box } from "@mui/material";
+import { styles } from "@/src/styles/event.mui.styles.mjs";
 
-  // TODO - create a lambda function that can update user groups - for now it is manual
-  return <h1>Organisers</h1>;
+import { useRouter } from "next/navigation";
+import { useUser } from "@/src/utils/userContext";
+
+function Organisers() {
+  const router = useRouter();
+  const { loggedIn } = useUser();
+
+  const isLoggedIn = loggedIn();
+
+  return (
+    <Box sx={{ maxWidth: "1200px" }}>
+      <Typography variant="h3" component="h1" sx={styles.mainTitle}>
+        Organisers
+      </Typography>
+      <Grid container={true} spacing={{ xs: 2, sm: 3, md: 4 }} sx={styles.gridPadding}>
+        <Grid size={{ xs: 12, lg: 6 }}>
+          <EventRegistrationForm router={router} isLoggedIn={isLoggedIn} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default Organisers;
